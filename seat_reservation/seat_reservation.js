806fd94b368c0bb2ac616d1a6de2705776d1dfd4
SAMRIDDHI JAIN <63547318+SamriddhI-ndia@users.noreply.github.com>
2021-06-22 23:18:30 +0530

    Day_9

---
+++ seat_reservation/seat_reservation.js
@@ -0,0 +1,37 @@
+const maint=document.querySelector('.blo');
+var h;
+var v;
+console.log(maint)
+for(var i=0;i<10;i++){
+
+    v=document.createElement('div');
+    v.className='verti';
+    
+    for(var j=0;j<10;j++){
+        console.log(i);
+        h=document.createElement('div');
+        h.className='hori';
+        v.appendChild(h);
+       
+    }
+    maint.appendChild(v);
+}
+var a=0;
+var hh=document.querySelectorAll('.hori');
+ for(let i=0;i<400;i++){
+    hh[i].addEventListener('click',()=>{
+      
+       if(hh[i].classList.contains('reserved')){
+        hh[i].classList.remove('reserved');
+        a-=1;
+       }
+       else{
+        hh[i].classList.add('reserved');
+        a+=1;
+        
+       }
+       console.log(a);
+       document.querySelector('.here').innerHTML=a;
+    });
+ }
+ 
